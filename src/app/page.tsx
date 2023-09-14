0b83924c55e729dbc23e43b18fd06617e43448c8
Christian Toledana <toledana.ian@gmail.com>
2023-09-14 09:32:59 +0800

    Added nft card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,20 @@
+import { NftCardComponent } from '@/components/NftCardComponent';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="max-w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
-        <img
-          className="rounded-t-lg"
-          src="/docs/images/blog/image-1.jpg"
-          alt=""
-        />
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <div className={'btn btn-primary mx-auto'}>Read more</div>
-        </div>
-      </div>
+    <main className="flex flex-wrap justify-center gap-4 p-4">
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
+      <NftCardComponent />
     </main>
   );
 }
---
+++ src/components/NftCardComponent.tsx
@@ -0,0 +1,36 @@
+export const NftCardComponent = () => {
+  return (
+    <>
+      <div className="max-w-xs border rounded-lg shadow bg-gray-800 border-gray-700">
+        <div
+          className={
+            'aspect-video bg-black w-full rounded-t-lg text-center flex flex-col justify-center'
+          }
+        >
+          <video
+            className="aspect-video w-full"
+            autoPlay={true}
+            loop={true}
+            muted={true}
+            src={
+              'https://ipfs.filebase.io/ipfs/QmZ5LiJvxhuSUx2RKcem2CL95Vx5wgKHqYziQWekqpwBwP'
+            }
+          />
+          {/*Video Preview*/}
+        </div>
+        <div className="flex flex-col p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <div className={'btn btn-primary mx-auto w-full'}>Mint</div>
+        </div>
+      </div>
+    </>
+  );
+};
