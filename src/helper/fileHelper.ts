0c199beccb32fe6ca37689372a2d5646cb568d34
Christian Toledana <toledana.ian@gmail.com>
2023-10-29 22:49:08 +0800

    Added helper file for file upload timeout

--- src/helper/fileHelper.ts
+++ src/helper/fileHelper.ts
@@ -8,3 +8,17 @@ export const removeExtension = (filename: string): string => {
   // Otherwise, return the substring from the start of the filename to the last '.'
   return filename.substring(0, lastDotIndex);
 };
+
+export const getRecommendedFileUploadTimeout = (file: File) => {
+  const fileSizeInBytes = file.size;
+
+  // Assuming an upload speed of 62.5 bytes/ms (0.5 Mbps)
+  const bytesPerMillisecond = 125;
+
+  // Calculate the recommended timeout based on the file size
+  const timeoutInMilliseconds = fileSizeInBytes / bytesPerMillisecond;
+
+  // Add a buffer of 20% to the calculated timeout for potential network fluctuations
+  const buffer = 0.2;
+  return timeoutInMilliseconds * (1 + buffer);
+};
